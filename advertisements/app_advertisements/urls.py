cc5a42715b8b523b6c48b2530f958999a9b1b164
Иван Иванович <lyubimkow@gmail.com>
2023-08-13 18:52:21 +0300

    Код с лекции №10.

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, page1, top_sellers
+from .views import index, page1, top_sellers, advertisement_post
 
 
 urlpatterns = [
     path('', index, name='main-page'),
     path('page1/', page1),
-    path('top-sellers/', top_sellers, name='top-sellers')
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='adv-post'),
 ]
