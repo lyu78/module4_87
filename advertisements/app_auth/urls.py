132b74aa27dfbe99a3c5d81aae2e17023f4a6c65
Иван Иванович <lyubimkow@gmail.com>
2023-08-19 18:09:13 +0300

    Код 11 лекция. Конец.

--- advertisements/app_auth/urls.py
+++ advertisements/app_auth/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from .views import (
+    login_view, logout_view,
+    register_view, profile_view
+)
+
 
 urlpatterns = [
-    
+    path("login/", login_view, name='login'),
+    path("profile/", profile_view, name='profile'),
+    path("logout/", logout_view, name='logout'),
+    path("register/", register_view, name='register'),
 ]
--- advertisements/app_auth/views.py
+++ advertisements/app_auth/views.py
@@ -1,2 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
+
+def profile_view(request):
+    return render(request, 'app_auth/profile.html')
+
+
+def login_view(request):
+
+    redirect_url = reverse('profile')
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect(redirect_url)
+        else:
+            return render(request, 'app_auth/login.html')
+
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect(redirect_url)
+    return render(request, 'app_auth/login.html', {"error": "Пользователь не найден!"})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('login'))
+
+
+def register_view(request):
+    return render(request, 'app_auth/register.html')
