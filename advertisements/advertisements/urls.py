b3150391c9a746249f069ed5743b6aa92eec3f8c
Иван Иванович <lyubimkow@gmail.com>
2023-08-19 16:50:22 +0300

    Код 11 лекция. Перерыв.

--- advertisements/advertisements/urls.py
+++ advertisements/advertisements/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('myauth/', include('app_auth.urls')),
     path('', include('app_advertisements.urls')),
     path('lesson_4', include('app_lesson_4.urls'))
 ]
@@ -14,4 +15,4 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
-    )
+)
